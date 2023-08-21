95358d7633bc505816b3fa34c555ee214851de5b
Elybourland <JEBourland@yahoo.com>
2023-08-20 19:27:32 -0700

    adding edit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,8 @@ class ProductController extends Controller
 {
     public function index()
     {
-        return view('products.index');
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
     }
 
     public function create()
@@ -29,5 +30,24 @@ class ProductController extends Controller
         $newProduct = Product::create($data);
 
         return redirect(route('product.index'));
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', ['product' => $product]);
+    }
+
+    public function update(Product $product, Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
+
+        $product->update($data);
+
+        return redirect(route('product.index'))->with('success', 'Product Updated Successfully!');
     }
 }
